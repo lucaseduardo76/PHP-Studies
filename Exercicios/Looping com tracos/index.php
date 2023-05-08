b83bedd64df6b4a28f82ece9c6ead8bd217216bb
Lucas Eduardo <lesds2001@gmail.com>
2023-05-07 23:15:22 -0300

    Exercices

---
+++ Exercicios/Looping com tracos/index.php
@@ -0,0 +1,10 @@
+<?php
+    for($i=1;$i<=10;$i++){
+
+        for($o=1; $o<=10; $o++){
+            echo "-";
+        };
+        echo "<br/>";
+    };
+
+    //exercicio é fazer 10 linha com 10 traços em cada
--- Looping Foreach/index.php
+++ Looping Foreach/index.php
@@ -9,4 +9,8 @@
 
     foreach($ingredientes as $item){
         echo "Item: ".$item."<br/>";
+    } 
+    echo "<br/> <br/>";
+    foreach($ingredientes as $chave => $valor){
+        echo "O item $valor está na posição: $chave </br>";
     }
