2d0f19860fecfdc7cf7388289981f4787fb7de7e
Lucas Eduardo <lesds2001@gmail.com>
2023-05-08 23:29:50 -0300

    changing if

--- WorkingIF/index.php
+++ WorkingIF/index.php
@@ -4,6 +4,6 @@ if($idade < 18) {
     echo "Menor de idade";
 }else if($idade >= 18 && $idade < 100){
     echo "Maior de idade";
-}else if($idade >= 100){
+}else if($idade >= 90){
     echo "Uma múmia";
 };
